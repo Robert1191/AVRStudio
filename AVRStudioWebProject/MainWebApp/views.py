a7561248242938c13746d03a7922e9b295d54288
Robert Le <dsz8022@autuni.ac.nz>
2018-05-11 12:37:19 +1200

    AVS

--- AVRStudioWebProject/MainWebApp/views.py
+++ AVRStudioWebProject/MainWebApp/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,"MainWebApp/index.html")
+
+def about(request):
+    return render(request,"MainWebApp/about.html")
