c7260ad0ed416b2e2faa86ff4e83d88962f96590
Robert1191 <robert01011991@gmail.com>
2018-05-17 01:23:38 +1200

    style

--- AVRStudioWebProject/MainWebApp/static/MainWebApp/jquery/mainFunc.js
+++ AVRStudioWebProject/MainWebApp/static/MainWebApp/jquery/mainFunc.js
@@ -1,12 +1,64 @@
 
 $(document).ready(function(){
+
+
+    var isHiddenMenuOpen = false;
+    $('#hiddenMenuButton').on( "click", function(e) {
+        if(!isHiddenMenuOpen){
+            openHiddenMenu();
+        }else{
+            closeHiddenMenu();
+        }
+    });
+
+    $('#hiddenLayer').on( "click", function(e) {
+            closeHiddenMenu();
+    });
+
+    $('.mainSideBar').on( "click", function(e) {
+         if(isHiddenMenuOpen){
+            closeHiddenMenu();
+        }
+    });
+
+    $('.sideBarHeader').on( "click", function(e) {
+         if(isHiddenMenuOpen){
+            closeHiddenMenu();
+        }
+    });
+
+    $('.sideBarBody').on( "click", function(e) {
+         if(isHiddenMenuOpen){
+            closeHiddenMenu();
+        }
+    });
+
+    $('.sideBarFooter').on( "click", function(e) {
+         if(isHiddenMenuOpen){
+            closeHiddenMenu();
+        }
+    });
+
+    function openHiddenMenu(){
+        $('#hiddenLayer').css("visibility", "visible");
+        $('.mainContent').css("margin-left", 400);
+        $('.mainSideBar').css("margin-left", 0);
+        isHiddenMenuOpen = true;
+    }
+
+    function closeHiddenMenu(){
+        $('#hiddenLayer').css("visibility", "hidden");
+        $('.mainContent').css("margin-left", 0);
+        $('.mainSideBar').css("margin-left", -400);
+        isHiddenMenuOpen = false;
+    }
   /* Set selected side bar button*/
-            var i;
-            for (i = 0; i < $(".sideBarBody").children().length; i++) {
-                if($($(".sideBarBody").children()[i]).attr('href') == $.trim($("#hiddenCurrentUrl").text())){
-                    $($(".sideBarBody").children()[i]).addClass( "selectedSideBarButton" );
-                }
-            }
+    var i;
+    for (i = 0; i < $(".sideBarBody").children().length; i++) {
+        if($($(".sideBarBody").children()[i]).attr('href') == $.trim($("#hiddenCurrentUrl").text())){
+            $($(".sideBarBody").children()[i]).addClass( "selectedSideBarButton" );
+        }
+    }
 
    /* Switch page function*/
     if  ($.trim($("#hiddenCurrentUrl").text()) == "/"){
@@ -34,3 +86,4 @@ $(document).ready(function(){
         });
     }
 });
+
